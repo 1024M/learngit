e4aef86eea74ee66c9168cf81bf6e6b6d4ea1362
chalie <978051245@qq.com>
2017-07-30 20:24:34 +0800

    panic

--- Hellogit.java
+++ Hellogit.java
@@ -3,7 +3,8 @@ class  Hellogit
 	public static void main(String[] args) 
 	{
 		System.out.println("Hello git  and Hello world, this is the 5th edtion.");
-		System.out.ptintln(" manage changing");
+		System.out.ptintln(" manage changing ????");
+
 
 	}
 }
