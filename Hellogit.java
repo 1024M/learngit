49cab28da884d56ed8898acffb222f603ab5f67c
chalie <978051245@qq.com>
2017-07-30 20:05:55 +0800

    comit license and 5th edtion

--- Hellogit.java
+++ Hellogit.java
@@ -2,7 +2,7 @@ class  Hellogit
 {
 	public static void main(String[] args) 
 	{
-		System.out.println("Hello git  and Hello world, this is the 4th edtion.");
+		System.out.println("Hello git  and Hello world, this is the 5th edtion.");
 
 	}
 }
